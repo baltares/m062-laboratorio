ff7a492136b355d2b2a9174aa5039f37b8582719
Barbara Altares <b.altares@gmail.com>
2022-04-28 18:07:03 +0200

    v0428

---
+++ app.js
@@ -0,0 +1,17 @@
+// DEFINING VARIABLES
+let plainAlphabet = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
+let encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
+
+function encrypt() {
+
+    let productsList = document.getElementById("products-list");
+
+}
+
+function decrypt() {
+
+}
+
+function transform(input, output) {
+
+}
