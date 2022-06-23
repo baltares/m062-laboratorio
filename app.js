757713b978a0c200dceaa4cf46e632352c606341
Barbara Altares <b.altares@gmail.com>
2022-06-23 17:28:52 +0200

    v0623

--- app.js
+++ app.js
@@ -2,16 +2,34 @@
 let plainAlphabet = "abcdefghijklmnopqrstuvwxyz:()!¡,'";
 let encryptedAlphabet = "qw,ert(yuio'pa:sdfg!hjklz¡xcv)bnm";
 
-function encrypt() {
+let plainTextarea = document.getElementById("plain-text");
+let encryptedTextarea = document.getElementById("encrypted-text");
+let transformedString = "";
 
-    let productsList = document.getElementById("products-list");
+// Transform function
+var transform = function (inputTextarea, outputTextarea, inputAlphabet, outputAlphabet) {
+    let inputString = inputTextarea.value.toLowerCase();
+    for (let letter of inputString) {
+        inputLetterIndex = inputAlphabet.indexOf(letter);
+        transformedString += (inputLetterIndex == -1) ? letter : outputAlphabet[inputLetterIndex];
+    }
+    outputTextarea.value = transformedString;
+    transformedString = "";
+};
 
-}
 
-function decrypt() {
+// Buttons event binding
+// Testing 2 forms of calling functions with parameters inside addEventListener
+document.getElementById("encrypt-button")
+    .addEventListener("click", transform.bind(null, plainTextarea, encryptedTextarea, plainAlphabet, encryptedAlphabet), false);
+document.getElementById("decrypt-button")
+    .addEventListener("click", function () {
+        transform(encryptedTextarea, plainTextarea, encryptedAlphabet, plainAlphabet);
+    }, false);
 
-}
+// Reset button
+document.getElementById("reset-button").addEventListener("click", () => {
+    plainTextarea.value = "";
+    encryptedTextarea.value = "";
+});
 
-function transform(input, output) {
-
-}
